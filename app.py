d74a4942a2bc2d9776a9b9dba65969d65de3fa60
Pollo-30 <vmsan30@gmail.com>
2026-04-21 08:35:04 -0600

    Update home route to display version 2.2

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Victor Alejandro --version 2.1"
+    return """
+┌───────────────────────────────┐
+│        Victor Alejandro        │
+│           versión 2.2          │
+└───────────────────────────────┘
+"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
